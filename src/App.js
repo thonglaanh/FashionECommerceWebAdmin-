3a1a428ec12e527642455116996a7067cb73ffa3
nguyenhuuthong <thonglaanh1011@gmail.com>
2023-10-16 14:05:48 +0700

    Firt commit : Login screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import DashBroad from "./pages/DashBroad";
+import Login from "./pages/Login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashbroad" element={<DashBroad />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/DashBroad.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/DashBroad.css'
+const DashBroad = () => {
+    return (
+        <div>DashBroad</div>
+    )
+}
+
+export default DashBroad
